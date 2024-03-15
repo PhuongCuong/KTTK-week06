72478f51e5cdb81736e7dda9a30121fa9f92a061
PhuongCuong <20078421.cuong@student.iuh.edu.vn>
2024-03-15 17:12:05 +0700

    upload

---
+++ service-product/src/main/java/com/example/serviceproduct/modules/Product.java
@@ -0,0 +1,19 @@
+package com.example.serviceproduct.modules;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nameProduct;
+    private int soLuong;
+}
---
+++ service-user/src/main/java/com/example/serviceuser/modules/User.java
@@ -0,0 +1,18 @@
+package com.example.serviceuser.modules;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
